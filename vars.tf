ccc45f0f3e47dba1ad14fd5acaeac7adb3e84678
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-29 11:33:14 +0530

    Terraform

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "env" {}
+variable "tags" {}
+variable "rds_type" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+variable "engine_family" {}
+variable "db_port" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "skip_final_snapshot" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "instance_count" {}
+variable "instance_class" {}
